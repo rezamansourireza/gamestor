e19f8509815903923bc8d18549425b6924556971
rezamansourireza <rema49202@gmail.com>
2024-08-01 22:53:55 +0330

    aterialize

---
+++ js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems,{
+      edge:'right'
+    });
+
+    var carouselElems = document.querySelectorAll('.carousel.top_carousel');
+    M.Carousel.init(carouselElems, {
+        fullWidth: true,
+        indicators: true,
+        onCycleTo: function(){
+            let elems = document.querySelectorAll('.top_text');
+            for (const item of elems) {
+                item.classList.remove('isShow');
+                setTimeout(function(){
+                    item.classList.add('isShow')
+                },700)
+            }
+        },
+    });
+
+
+  });
