36c17f16e5a72d5b5fe39348e2d9149441815df8
rezamansourireza <rema49202@gmail.com>
2024-08-04 21:52:36 +0330

    color and flaouting btn

--- js/app.js
+++ js/app.js
@@ -19,5 +19,11 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'top',
+      hoverEnabled: false
+    });
+
 
   });
