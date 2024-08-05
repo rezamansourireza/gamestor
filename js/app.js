4ffe5a350eed2476aa5cf6d5f5b2ea993768d490
rezamansourireza <rema49202@gmail.com>
2024-08-05 13:55:46 +0330

    new update

--- js/app.js
+++ js/app.js
@@ -1,29 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems,{
-      edge:'right'
-    });
-
-    var carouselElems = document.querySelectorAll('.carousel.top_carousel');
-    M.Carousel.init(carouselElems, {
-        fullWidth: true,
-        indicators: true,
-        onCycleTo: function(){
-            let elems = document.querySelectorAll('.top_text');
-            for (const item of elems) {
-                item.classList.remove('isShow');
-                setTimeout(function(){
-                    item.classList.add('isShow')
-                },700)
-            }
-        },
-    });
-
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'top',
-      hoverEnabled: false
-    });
-
-
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+    edge: 'right'
   });
+
+  var carouselElems = document.querySelectorAll('.carousel.top_carousel');
+  M.Carousel.init(carouselElems, {
+      fullWidth: true,
+      indicators: true,
+    },
+    setInterval(()=> {
+      M.Carousel.getInstance(carouselElems[0]).next()
+    }, 5000)
+  );
+
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'top',
+    hoverEnabled: false
+  });
+  
+
+
+});
